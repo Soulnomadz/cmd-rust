f49bc57cbcc9a3abfa280b1bd6d9edcced956f05
root <root@localhost.localdomain>
2024-06-01 23:28:22 -0400

    add catr support

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,30 +1,12 @@
 use anyhow::Result;
 use clap::Parser;
 
-pub fn run() -> Result<()> {
-    println!("{:?}", get_args());
-    Ok(())
-}
-
-fn get_args() -> Result<Config> {
-    let args = Args::parse();
-    Ok(Config {
-        files: args.files,
-        number_lines: args.number_lines,
-        number_nonblank_lines: args.number_nonblank_lines,
-    })
-}
-
-#[derive(Debug)]
-pub struct Config {
-    files: Vec<String>,
-    number_lines: bool,
-    number_nonblank_lines: bool,
-}
+use std::io::{self, BufReader, BufRead};
+use std::fs::File;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
-struct Args {
+pub struct Args {
     #[arg(default_value = "-")]
     files: Vec<String>,
 
@@ -40,3 +22,63 @@ struct Args {
     )]
     number_nonblank_lines: bool,
 }
+
+
+pub fn run(args: Args) -> Result<()> {
+    // dbg!(config);
+    for f in args.files {
+        // dbg!(&f);
+        match open(&f) {
+            Err(err) => eprintln!("{}: {}", f, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?;
+
+                    if args.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if args.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(
+            Box::new(BufReader::new(io::stdin()))
+        ),
+        _ => Ok(
+            Box::new(BufReader::new(File::open(filename)?))
+        ),
+    }
+}
+
+// #[derive(Debug)]
+// pub struct Config {
+//     files: Vec<String>,
+//     number_lines: bool,
+//     number_nonblank_lines: bool,
+// }
+
+// pub fn get_args() -> Result<Config> {
+//     let args = Args::parse();
+//     Ok(Config {
+//         files: args.files,
+//         number_lines: args.number_lines,
+//         number_nonblank_lines: args.number_nonblank_lines,
+//     })
+// }
+
+
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,10 @@
+use clap::Parser;
+use catr::*;
+
 fn main() {
-    if let Err(e) = catr::run() {
+    if let Err(e) = 
+        run(Args::parse()) {
+        // catr::get_args().and_then(catr::run) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
